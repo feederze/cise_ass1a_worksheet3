72f511fdfb91518e5efe4dcc11dee9667e269682
feederze <46537990+feederze@users.noreply.github.com>
2022-04-03 23:22:07 +1200

    another random update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-
 import CreateBook from './components/CreateBook';
 import ShowBookList from './components/ShowBookList';
 import ShowBookDetails from './components/ShowBookDetails';
